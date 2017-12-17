207ea6dc62cc19bf824799af3641da66385cde88
Alan Ramsey <ramseya@fastmail.com>
2017-12-17 18:55:47 -0500

    Style options page

--- src/options/view.jsx
+++ src/options/view.jsx
@@ -1,11 +1,13 @@
 import { h } from 'hyperapp';
 
+import './style.css';
+
 const UNITS = ['seconds', 'minutes', 'hours'];
 
 const capitalize = s => s.slice(0, 1).toUpperCase() + s.slice(1);
 
 const TimeUnit = ({ selected, setUnit }) => (
-    <select oninput={e => setUnit(e.target.value)}>
+    <select class="browser-style" oninput={e => setUnit(e.target.value)}>
         {UNITS.map(unit => (
             <option value={unit} selected={unit === selected}>
                 {capitalize(unit)}
@@ -15,21 +17,24 @@ const TimeUnit = ({ selected, setUnit }) => (
 );
 
 const Time = ({ time, remove, setUnit, setValue }) => (
-    <div>
+    <div class="browser-style entry">
         <input
+            class="browser-style duration-input"
             type="text"
             value={time.value === 0 ? '' : time.value}
             oninput={e => setValue(e.target.value)}
         />
         <TimeUnit selected={time.unit} setUnit={setUnit} />
-        <button onclick={remove}>Remove</button>
+        <button class="browser-style" onclick={remove}>
+            Remove
+        </button>
     </div>
 );
 
 const view = state => actions => (
     <section>
         <h1>Intervals</h1>
-        <div>
+        <div class="intervals">
             {state.times.map((time, index) => (
                 <Time
                     time={time}
@@ -39,10 +44,17 @@ const view = state => actions => (
                 />
             ))}
             <div>
-                <button onclick={actions.addEntry}>Add</button>
+                <button class="browser-style" onclick={actions.addEntry}>
+                    Add
+                </button>
             </div>
             <div>
-                <button onclick={actions.save}>Save</button>
+                <button
+                    class="browser-style save-button"
+                    onclick={actions.save}
+                >
+                    Save
+                </button>
             </div>
         </div>
     </section>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     module: {
@@ -8,6 +9,12 @@ module.exports = {
             test: /\.jsx?$/,
             use: 'babel-loader',
             exclude: join(__dirname, 'node_modules'),
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader',
+            }),
         }]
     },
     resolve: {
@@ -28,5 +35,6 @@ module.exports = {
             chunks: ['options'],
             title: 'Options',
         }),
+        new ExtractTextPlugin('style.css'),
     ],
 };
